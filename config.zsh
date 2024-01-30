aef9a4751e7e5e5bf44b25b24c5d8a8fea011158
Eslam Allam <eslamallam73@gmail.com>
2024-01-30 18:34:15 +0300

    feat: added autossh completion

--- config.zsh
+++ config.zsh
@@ -4,10 +4,14 @@ else
 	if [ -x "$(command -v nvim)" ]; then
 		export EDITOR='nvim'
 	else
-		export EDITOR='vi' 
+		export EDITOR='vi'
 	fi
  fi
 
 # Zsh Autocpmplete
 ## Make Enter submit the command line straight from the menu.
 #bindkey -M menuselect '\r' .accept-line
+
+# AutoSsh
+## Copy suggestions from ssh
+compdef autossh=ssh
--- env.zsh
+++ env.zsh
@@ -2,3 +2,4 @@ source /etc/profile.d/gradle.sh
 source /etc/profile.d/maven.sh
 export XDG_CONFIG_HOME=~/.config
 export PATH=$PATH:$(go env GOPATH)/bin
+export PATH=$HOME/.local/bin:$PATH
