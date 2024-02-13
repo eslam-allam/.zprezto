a0587b4d3100d623c344a6d34112ab824761f8f5
Eslam Allam <eslamallam73@gmail.com>
2024-02-13 13:10:56 +0300

    feat: change zsh suggestions colour

--- env.zsh
+++ env.zsh
@@ -1,6 +1,18 @@
+#Sources
+
 source /etc/profile.d/gradle.sh
 source /etc/profile.d/maven.sh
+
+# Config
+
 export XDG_CONFIG_HOME=~/.config
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#bcbcbc,bold,underline"
+
+# Binraries
+
 export PATH=$PATH:$(go env GOPATH)/bin
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
+
+
+
