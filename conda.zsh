b28a6394d0e9451bcee55e0b9a893faf1479059c
Eslam Allam <eslamallam73@gmail.com>
2025-02-18 01:59:26 +0300

    fix: Remove conda alias after loading

--- conda.zsh
+++ conda.zsh
@@ -2,16 +2,13 @@ DISTRO=$(lsb_release -si)
 
 conda ()
 {
-  if [[ -z "$CONDA_ACTIVE" ]]; then
-    if [[ $DISTRO == "Arch" ]]; then
-      source "${ZDOTDIR:-$HOME/.zprezto/conda.arch.zsh}"
-    else
-      source "${ZDOTDIR:-$HOME/.zprezto/conda.generic.zsh}"
-    fi
-    export CONDA_ACTIVE=1
-    conda "$@"
+  if [[ $DISTRO == "Arch" ]]; then
+    source "${ZDOTDIR:-$HOME/.zprezto/conda.arch.zsh}"
   else
-    conda "$@"
+    source "${ZDOTDIR:-$HOME/.zprezto/conda.generic.zsh}"
   fi
+  export CONDA_ACTIVE=1
+  unset -f conda
+  conda "$@"
 }
 
