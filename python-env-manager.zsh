93835a117b45ba0dbc1d0d6c7736f47fbcdef675
Eslam Allam <eslamallam73@gmail.com>
2026-03-30 03:24:24 +0300

    feat: Add pixi to list of supported python managers

--- python-env-manager.zsh
+++ python-env-manager.zsh
@@ -2,6 +2,8 @@ if command -v micromamba &> /dev/null; then
   source "${ZDOTDIR:-$HOME/.zprezto/micromamba.zsh}"
   elif command -v conda &> /dev/null; then
     source "${ZDOTDIR:-$HOME/.zprezto/conda.zsh}"
+  elif command -v pixi &> /dev/null; then
+    # Do nothing
   else
-    echo "Conda and Micromamba not found!!!"
+    echo "Conda, Micromamba, Pixi not found!!!"
 fi
